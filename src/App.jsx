accb65f6ba9b4f8ab87f822f042922f727acbc7c
Jose Garcia <garciacolin@hotmail.com>
2024-02-07 17:01:17 -0600

    Displaying a loader indicator & handling errorwith error elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import AppLayout from './components/AppLayout.jsx';
+import Error from './components/Error.jsx';
 import Home from './pages/Home.jsx';
 import Menu, {loader as menuLoader} from './pages/Menu.jsx';
 import Cart from "./pages/Cart.jsx";
@@ -10,9 +11,10 @@ import Order from "./pages/Order.jsx";
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/menu", element: <Menu />, loader: menuLoader, },
+      { path: "/menu", element: <Menu />, errorElement: <Error />, loader: menuLoader },
       { path: "/cart", element: <Cart />, },
       { path: './order/new', element: <CreateOrder /> },
       { path: './order/orderId', element: <Order /> }
--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,10 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./Header.jsx";
 import CartOverview from './cart/CartOverview.jsx';
+import Loader from "./Loader.jsx";
 
 const AppLayout = () => {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading"
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
+      
       <Header />
 
       <main>
