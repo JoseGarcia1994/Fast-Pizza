eb875c93787cdf691ff0eb6bbe96c9d321de1e43
Jose Garcia <garciacolin@hotmail.com>
2024-02-07 16:27:41 -0600

    Fetching Data with React Router: Loaders

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import AppLayout from './components/AppLayout.jsx';
 import Home from './pages/Home.jsx';
-import Menu from './pages/Menu.jsx';
+import Menu, {loader as menuLoader} from './pages/Menu.jsx';
 import Cart from "./pages/Cart.jsx";
 import CreateOrder from './pages/CreateOrder.jsx';
 import Order from "./pages/Order.jsx";
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
     element: <AppLayout />,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/menu", element: <Menu /> },
+      { path: "/menu", element: <Menu />, loader: menuLoader, },
       { path: "/cart", element: <Cart />, },
       { path: './order/new', element: <CreateOrder /> },
       { path: './order/orderId', element: <Order /> }
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,10 +1,23 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../services/apiRestaurant.js";
+import  MenuItem from "../components/menu/MenuItem.jsx";
 
 const Menu = () => {
+  const menu = useLoaderData();
   return (
-    <div>
-      Menu
-    </div>
+    <ul>
+      {
+        menu.map(pizza => (
+          <MenuItem key={pizza.id} pizza={pizza}/> 
+        ))
+      }
+    </ul>
   );
 };
+
+export const loader = async () => {
+  const menu = await getMenu();
+  return menu;
+}
 
 export default Menu;
