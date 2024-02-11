a6b371884dc823606e710011776986179d280bd5
Jose Garcia <garciacolin@hotmail.com>
2024-02-10 20:02:57 -0600

    Modeling the Cart Slice

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './slices/user.slice.js';
+import cartReducer from './slices/cart.slice.js';
 
 export default configureStore({
   reducer: {
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer,
 	}
 })
