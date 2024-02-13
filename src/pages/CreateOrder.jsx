5ccc21d7b92239f1b268082d4fcbfb1d96d487e5
Jose Garcia <garciacolin@hotmail.com>
2024-02-12 19:33:18 -0600

    Thunks: fetchAddress

--- src/pages/CreateOrder.jsx
+++ src/pages/CreateOrder.jsx
@@ -1,12 +1,13 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../services/apiRestaurant.js";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { clearCart, getCart, getTotalCartPrice } from "../store/slices/cart.slice.js";
 import { formatCurrency } from '../utils/helpers.js';
 import { useState } from "react";
 import Button from "../components/Button.jsx";
 import EmptyCart from '../components/cart/EmptyCart.jsx';
 import store from '../store';
+import { fetchAddress } from "../store/slices/user.slice.js";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -22,6 +23,7 @@ const CreateOrder = () => {
   const isSubmitting = navigation.state === "submitting";
 
   const formErrors = useActionData();
+  const dispatch = useDispatch();
 
   const cart = useSelector(getCart);
   const totalCartPrice = useSelector(getTotalCartPrice);
@@ -34,6 +36,7 @@ const CreateOrder = () => {
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
 
+      <button onClick={() => dispatch(fetchAddress())}>Get Position</button>
       <Form method="POST">
         <div className="flex flex-col gap-2 mb-5 sm:flex-row sm:items-center">
           <label className="sm:basis-40">
